9fe290bb87a7824b2a0fd287d0305f1c29c04116
Plamen Boudinov <boudinov@gmail.com>
2016-11-02 11:33:13 +0200

    initial commit

---
+++ ContinuousJobGracefulShutdown/Functions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using System.IO;
+
+namespace ContinuousJobGracefulShutdown
+{
+    public static class Functions
+    {
+        [NoAutomaticTrigger]
+        public static async Task LoopTestFunction(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    Console.WriteLine("LoopTestFunction in loop");
+                    await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+                }
+                catch (TaskCanceledException)
+                {
+                    Console.WriteLine("LoopTestFunction entering Shutdown wait");
+                    //simulate some shutdown cleanup
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                }
+            }
+
+            Console.WriteLine("LoopTestFunction completed succesfully.");        
+        }
+
+        public static async Task TimerTestFunction([TimerTrigger("*/20 * * * * *", RunOnStartup = true)] TimerInfo info, CancellationToken token, TextWriter log)
+        {
+            Console.WriteLine("TimerTestFunction ran");
+            log.Write("TimerTestFunction ran");
+        }
+    }
+}
---
+++ ContinuousJobGracefulShutdown/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Azure.WebJobs;
+using System.Threading.Tasks;
+
+namespace ContinuousJobGracefulShutdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Environment.SetEnvironmentVariable("WEBJOBS_SHUTDOWN_FILE", "d:\\shutdown.test");
+
+            var config = new JobHostConfiguration();
+            config.UseTimers();
+
+            JobHost host = new JobHost(config);
+
+            MethodInfo methodInfo = typeof(Functions).GetMethod("LoopTestFunction");
+            var task = host.CallAsync(methodInfo);
+
+            host.RunAndBlock();
+
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine("While wating task: " + ex.Message);
+                ex.Handle(e => e is TaskCanceledException);
+            }
+        }
+    }
+}
